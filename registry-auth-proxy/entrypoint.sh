7baa15c1873c2d4cb921b39a98c0d1ebb426f41f
Jason Ho <git@hogt.me>
2022-04-19 22:08:36 -0700

    simple registry-auth-proxy

---
+++ registry-auth-proxy/entrypoint.sh
@@ -0,0 +1,23 @@
+SUBSTITUTES="$(python -c 'import os; print(",".join( "${" + x + "}" for x in os.environ ))')"
+echo "$SUBSTITUTES"
+function substitute {
+    mkdir -p /tmp/$1
+    envsubst "$SUBSTITUTES" < $1 > /tmp/$1/tmp.txt
+    cat /tmp/$1/tmp.txt > $2
+}
+
+substitute /home/container/config/nginx.conf /etc/nginx/conf.d/default.conf
+
+IFS=';'
+read -ra CRED <<< $AUTH_CREDENTIALS
+for i in "${CRED[@]}"; do
+    echo "->"
+    IFS=':'
+    read -ra PAIR <<< $i
+    htpasswd -b -B -c /auth.htpasswd "${PAIR[0]}" "${PAIR[1]}"
+    IFS=';'
+done
+IFS=' '
+
+echo "Ready."
+nginx -g "daemon off;"
