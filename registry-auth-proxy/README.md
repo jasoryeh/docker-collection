a0399fe8eb800d9f699897ba558f9f07a7629047
Jason Ho <git@hogt.me>
2022-04-27 08:53:09 -0700

    Better-ish way of configuring proxied config + adds ssl support

--- registry-auth-proxy/README.md
+++ registry-auth-proxy/README.md
@@ -2,3 +2,5 @@
 Uses NGINX to proxy the docker registry at `DOCKER_REGISTRY`, accepts BASIC authentication using the credentials specified in `AUTH_CREDENTIALS`.
 
 `AUTH_CREDENTIALS` example: "USERNAME:PASSWORD;someOtherUname:pass;abc:123"
+
+Enable SSL by mounting your certificate to `/etc/nginx/conf.d/certificate` and your private key to `/etc/nginx/conf.d/privatekey`, then set `SSL_ENABLED=true`
--- registry-auth-proxy/entrypoint.sh
+++ registry-auth-proxy/entrypoint.sh
@@ -6,11 +6,22 @@ function substitute {
     cat /tmp/$1/tmp.txt > $2
 }
 
-if [ -z "$PROXIED" ]; then
-    substitute /home/container/config/nginx.conf /etc/nginx/conf.d/default.conf
+substitute /home/container/config/nginx.conf /etc/nginx/conf.d/default.conf
+
+if [ ! -z "$SSL_ENABLED" ]; then
+    echo "Enabling SSL"
+    sed -i "s/#ssl://g" /etc/nginx/conf.d/default.conf
 else
+    echo "Using HTTP"
+    sed -i "s/#http://g" /etc/nginx/conf.d/default.conf
+fi
+
+if [ ! -z "$PROXIED" ]; then
     echo "Using proxied config"
-    substitute /home/container/config/nginx.conf.proxied /etc/nginx/conf.d/default.conf
+    sed -i 's/proxy_set_header  X-Real-IP         $remote_addr;/#proxy_set_header  X-Real-IP         $remote_addr;/g' /etc/nginx/conf.d/default.conf
+    sed -i 's/proxy_set_header  X-Forwarded-For   $proxy_add_x_forwarded_for;/#proxy_set_header  X-Forwarded-For   $proxy_add_x_forwarded_for;/g' /etc/nginx/conf.d/default.conf
+    sed -i 's/proxy_set_header  X-Forwarded-Proto $scheme;/#proxy_set_header  X-Forwarded-Proto $scheme;/g' /etc/nginx/conf.d/default.conf
+    sed -i 's/proxy_read_timeout                  900;/#proxy_read_timeout                  900;/g' /etc/nginx/conf.d/default.conf
 fi
 
 if [ ! -f /auth.htpasswd ]; then
